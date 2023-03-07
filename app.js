ca757163c592a88cda7051447dcd62a31bf9c5b5
margate23 <chrismargate231@gmail.com>
2023-03-07 17:14:15 +0800

    added more practice code

--- app.js
+++ app.js
@@ -24,7 +24,44 @@ window.console.log();
 global.console.log()
 */
 
-const logger = require('./logger.js');
+// const logger = require('./logger.js');
 // console.log(logger);
 
-logger.log("message");
+// logger.log("message");
+
+// PATH MODULE
+/*
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+*/
+
+
+// OS MODULE
+/*
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+// console.log("Total memory: " + totalMemory);
+// console.log("Free memory: " + freeMemory);
+
+// Template String
+// ES6 / ES2015: ECMAScript 6
+
+console.log(`Total memory: ${totalMemory}`);
+console.log(`Free memory: ${freeMemory}`);
+*/
+
+// FILE STORAGE MODULE
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
